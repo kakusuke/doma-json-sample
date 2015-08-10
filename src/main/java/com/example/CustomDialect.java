624e27f5413bd50d36e95e971cd26f38fb367e2c
角 輔 <tasuku@freemind.co.jp>
2015-08-10 20:44:02 +0900

    動くことを期待しているけど何故か動かない・・・

--- src/main/java/com/example/CustomDialect.java
+++ src/main/java/com/example/CustomDialect.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.sql.SQLException;
+
 import org.seasar.doma.jdbc.JdbcMappingFunction;
 import org.seasar.doma.jdbc.JdbcMappingHint;
 import org.seasar.doma.jdbc.JdbcMappingVisitor;
@@ -7,8 +9,6 @@ import org.seasar.doma.jdbc.dialect.PostgresDialect;
 import org.seasar.doma.jdbc.dialect.StandardDialect;
 import org.seasar.doma.jdbc.type.JdbcTypes;
 import org.seasar.doma.wrapper.StringWrapper;
-
-import java.sql.SQLException;
 
 /**
  * Created by kakusuke on 15/08/09.
@@ -22,7 +22,13 @@ public class CustomDialect extends StandardDialect {
     private JdbcMappingVisitor jdbcMappingVisitor = new PostgresDialect.PostgresJdbcMappingVisitor() {
         @Override
         public Void visitStringWrapper(StringWrapper wrapper, JdbcMappingFunction p, JdbcMappingHint q) throws SQLException {
-            return p.apply(wrapper, new PortableObjectType<>(JdbcTypes.STRING));
+            if (q.getDomainClass().map(DateRange.class::isAssignableFrom).orElse(false)) {
+              return p.apply(wrapper, new PortableObjectType<>(JdbcTypes.STRING));
+            }
+            if (q.getDomainClass().map(JsonObject.class::isAssignableFrom).orElse(false)) {
+              return p.apply(wrapper, new PortableObjectType<>(JdbcTypes.STRING));
+            }
+            return p.apply(wrapper, JdbcTypes.STRING);
 //            return super.visitStringWrapper(wrapper, p, q);
         }
     };
